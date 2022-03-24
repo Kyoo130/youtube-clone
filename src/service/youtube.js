9ab17c113257290e5a2a31c02d9d37586bf1a45b
Kyoo130 <kml9003@kakao.com>
2022-03-24 21:33:07 +0900

    :recycle: axios api 호출 코드 리팩토링

--- src/service/youtube.js
+++ src/service/youtube.js
@@ -1,29 +1,35 @@
-class Youtube {
+import axios from "axios";
+
+class youtube {
   constructor(key) {
-    this.key = key;
-    this.getRequestOptions = {
-      method: 'GET',
-      redirect: 'follow',
-    };
+    this.youtube = axios.create({
+      baseURL: 'https://youtube.googleapis.com/youtube/v3',
+      params: { key: key },
+    });
   }
 
   async mostPopular() {
-    const response = await fetch(
-      `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${this.key}`,
-      this.getRequestOptions
-    );
-    const result = await response.json();
-    return result.items;
+    const response = await this.youtube.get('videos', {
+      params: {
+        part: 'snippet',
+        chart: 'mostPopular',
+        maxResults: 25,
+      },
+    });
+    return response.data.items;
   }
 
   async search(query) {
-    const response = await fetch(
-      `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${this.key}`,
-      this.getRequestOptions
-    );
-    const result = await response.json();
-    return result.items.map(item => ({ ...item, id: item.id.videoId }));
+    const response = await this.youtube.get('search', {
+      params: {
+        part: 'snippet',
+        maxResults: 25,
+        type: 'video',
+        q: query,
+      },
+    });
+    return response.data.items.map(item => ({ ...item, id: item.id.videoId }));
   }
 }
 
-export default Youtube;
+export default youtube;
