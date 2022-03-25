805e28e2bd32c1ab60350c19c48d2dc1a710f0b0
Kyoo130 <kml9003@kakao.com>
2022-03-26 00:39:45 +0900

    :label: Detail 컴포넌트 import 코드 추가

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Header from "./Header";
 import VideoList from "./VideoList";
 import VideoItem from "./VideoItem";
+import Detail from "./Detail";
 
-export {Header, VideoList, VideoItem};
+export {Header, VideoList, VideoItem, Detail};
